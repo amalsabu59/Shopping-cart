f28f1666d57a6eac936be3c29f8cd7f22bb508cf
amalsabu59 <amalsabu59@gmail.com>
2022-01-08 14:02:36 +0530

    user page added

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    let products = [{
-            name: "iphone 11",
-            category: "mobile",
-            description: "fastest ever",
-            image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-black-select-2019?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1566956144418"
+    productHelpers.getAllProducts().then((products) => {
+        res.render('user/view-products', { admin: false, products })
+    })
 
-        },
-        {
-            name: "One plus 9",
-            category: "mobile",
-            description: "fastest ever",
-            image: "https://happimobiles.s3.ap-south-1.amazonaws.com/happi/WHBXBHXB-1616745334779.jpg"
-
-        },
-        {
-            name: "Samsung s22",
-            category: "mobile",
-            description: "fastest ever",
-            image: "https://www.corning.com/microsites/csm/gorillaglass/Samsung/CGG_Samsun_galaxys21_phantom.jpg"
-
-        },
-        {
-            name: "Realme",
-            category: "mobile",
-            description: "fastest ever",
-            image: "https://image01.realme.net/general/20210401/1617294011898.png"
-        },
-    ]
-    res.render('index', { title: 'Express', products, admin: false });
 });
-
 
 module.exports = router;
