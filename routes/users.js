0536315ef3bcd143aadb7b782049b10ac57d25bf
amalsabu59 <amalsabu59@gmail.com>
2022-01-08 19:47:31 +0530

    login and signup page created

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const res = require('express/lib/response');
 var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 
@@ -7,7 +8,15 @@ router.get('/', function(req, res, next) {
     productHelpers.getAllProducts().then((products) => {
         res.render('user/view-products', { admin: false, products })
     })
-
+    
 });
+router.get('/login', function(req, res, ) {
+    res.render('user/login')
+    })
+router.get('/signup',(req,res)=>{
+    res.render('user/signup')
+    
+})
+
 
 module.exports = router;
